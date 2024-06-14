a1af6b9a438d285ec2cd1b3ba334e9cbf5ddfcb9
Mushkir <mushkirmohamed@gmail.com>
2024-06-14 23:25:10 +0530

    feat: route structure designed

--- main.js
+++ main.js
@@ -9,6 +9,27 @@ app.get('/', (req, res) => {
     res.json({msg: "Hello students"})
 })
 
+// CRUD functionality of movies.
+// 1. R - Read all movies
+app.get('/movies', (req, res) => {
+
+})
+
+// 2. C - For creating / adding movies.
+app.post('/movies', () => {
+
+})
+
+// 3. U - For updating movie.
+app.put('/movies/:id', () => {
+
+})
+
+// D - For delete movie.
+app.delete('/movies/:id', () => {
+
+})
+
 
 app.listen(PORT, () => {
     console.log(`The server is running at http://localhost:${PORT}`);
