e41726c0b62a84ff9410bbf0ef9eb9d410938bc5
Mushkir <mushkirmohamed@gmail.com>
2024-06-15 18:09:31 +0530

    feat: connect to mongodb with env file

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 import express from "express";
 import movieRoutes from "./routes/movies.route.js";
+import connectDB from "./lib/db.js";
 
 
 const app = express();
 const PORT = 6969;
+
+
+connectDB();
 
 app.get('/', (req, res) => {
 
